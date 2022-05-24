c18c26987c80cc9380d9ea2c19dcf85f3e91970f
jari.moons1 <jari.moons@student.kdg.be>
2022-05-24 10:55:00 +0200

    made the svg floorplan clickable

--- src/svg/buildings/BuildingFloorPlan.tsx
+++ src/svg/buildings/BuildingFloorPlan.tsx
@@ -1,17 +1,22 @@
 import React from 'react'
-import { View } from 'react-native';
-import { Seat } from '../../seats/types/Seat';
-import { Companies } from './Companies';
+import {View} from 'react-native';
+import {Seat} from '../../seats/types/Seat';
+import {Companies} from './Companies';
 import XploreGroupFloorPlan from './Xplore Group/XploreGroupFloorPlan';
 
 interface BuildingFloorPlanProps {
     seats: Seat[];
     company: Companies;
     floorNumber: number;
+    date: Date;
 }
 
-export default function BuildingFloorPlan({ seats, company, floorNumber }: BuildingFloorPlanProps) {
+export default function BuildingFloorPlan({seats, company, floorNumber, date}: BuildingFloorPlanProps) {
     if (company == Companies.Xplore_Group)
-        return <XploreGroupFloorPlan seats={seats} floorNumber={floorNumber} />
-    return <View />
+        return (
+            <View>
+                <XploreGroupFloorPlan date={date} seats={seats} floorNumber={floorNumber}/>
+            </View>
+        )
+    return <View/>
 }
--- src/svg/buildings/Xplore Group/XploreGroupFloorPlan.tsx
+++ src/svg/buildings/Xplore Group/XploreGroupFloorPlan.tsx
@@ -6,11 +6,12 @@ import XploreGroupFloor1 from './XploreGroupFloor1';
 interface XploreGroupFloorPlanProps {
     seats: Seat[];
     floorNumber: number;
+    date: Date;
 }
 
-export default function XploreGroupFloorPlan({ seats, floorNumber }: XploreGroupFloorPlanProps) {
+export default function XploreGroupFloorPlan({ seats, floorNumber, date }: XploreGroupFloorPlanProps) {
     if (floorNumber == 1) {
-        return (<XploreGroupFloor1 seats={seats} />)
+        return (<XploreGroupFloor1 date={date} seats={seats} />)
     }
     return (<View />)
 }
