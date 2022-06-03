fee8101cf5e576cbc64a8e28eebb22cae8cedca6
jari.moons1 <jari.moons@student.kdg.be>
2022-06-03 15:16:42 +0200

    added loading screen

--- src/login/hooks/useLogin.ts
+++ src/login/hooks/useLogin.ts
@@ -3,8 +3,10 @@ import AxiosClient from "../../utils/AxiosClient";
 
 export default function useLogin() {
     const [loggedIn, setLoggedIn] = useState<boolean>(false);
+    const [loading, setLoading] = useState<boolean>(false);
 
     function checkLoggedIn() {
+        setLoading(true)
         return AxiosClient.get("/api/healthcheck")
             .then((response) => {
                 if (response.status >= 300)
@@ -14,11 +16,12 @@ export default function useLogin() {
             })
             .catch(() => {
                 setLoggedIn(false)
-            });
+            }).finally(() => setLoading(false));
     }
 
     return {
         loggedIn,
         checkLoggedIn,
+        loading
     };
 }
